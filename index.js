7ee8800b50a350d93a111f27d632933ccde3e9fe
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-10-10 18:14:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ Titanic.addEventListener("click", (e) => {
   }
 )
 
-const Terminator 2 = document.getElementById("Titanic")
+const Terminator = document.getElementById("Terminator 2")
 
 Titanic.addEventListener("click", (e) => {
-  document.getElementById("title").innerHTML = movies["Titanic"]["title"]
-  document.getElementById("director").innerHTML = movies["Titanic"]["director"]
-  document.getElementById("genre").innerHTML = movies["Titanic"]["genre"]
-  document.getElementById("filmRating").innerHTML = movies["Titanic"]["filmRating"]
-  document.getElementById("poster").innerHTML = movies["Titanic"]["poster"]
-  document.getElementById("description").innerHTML = movies["Titanic"]["description"]
-  document.getElementById("audienceScore").innerHTML = movies["Titanic"]["audienceScore"]
-  document.getElementById("case").innerHTML = movies["Titanic"]["cast"]
+  document.getElementById("title").innerHTML = movies["Terminator"]["title"]
+  document.getElementById("director").innerHTML = movies["Terminator"]["director"]
+  document.getElementById("genre").innerHTML = movies["Terminator"]["genre"]
+  document.getElementById("filmRating").innerHTML = movies["Terminator"]["filmRating"]
+  document.getElementById("poster").innerHTML = movies["Terminator"]["poster"]
+  document.getElementById("description").innerHTML = movies["Terminator"]["description"]
+  document.getElementById("audienceScore").innerHTML = movies["Terminator"]["audienceScore"]
+  document.getElementById("case").innerHTML = movies["Terminator"]["cast"]
 
   }
 )
