60de458e92f5842a581545fc9d6a3dbeb78cd73d
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-10-10 18:21:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ Titanic.addEventListener("click", (e) => {
   document.getElementById("poster").innerHTML = movies["Titanic"]["poster"]
   document.getElementById("description").innerHTML = movies["Titanic"]["description"]
   document.getElementById("audienceScore").innerHTML = movies["Titanic"]["audienceScore"]
-  document.getElementById("case").innerHTML.querySelectorAll (movies["Titanic"]["cast"])
+  document.getElementById("case").innerHTML = movies["Titanic"]["cast"]
   }
 )
 
