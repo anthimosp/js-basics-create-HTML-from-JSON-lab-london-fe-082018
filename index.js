d585ac70ce4418b2511f5d1c1373cf56781acbb3
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-10-10 17:49:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentloaded", (e) => {
   console.log(movies)
   // invoking a function here will make sure it happens on page load
   // your code here
-
+const Titanic = 
   Titanic.addEventListener("click", (e) => {
   
 }
