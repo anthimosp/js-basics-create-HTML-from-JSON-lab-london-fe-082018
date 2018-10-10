72702f9e8c8a7c14f33600f502f7a678229859b5
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-10-10 17:25:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentloaded", (e) => {
   console.log(movies)
   // invoking a function here will make sure it happens on page load
   // your code here
-  
+  function init
 });
